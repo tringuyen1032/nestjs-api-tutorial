113377e800c54ba2ac5e4e26f41243d6b20733f6
Nguyen Minh Tri <trinm.dev.1112@gmail.com>
2022-08-31 22:03:27 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
 })
 export class AppModule {}
